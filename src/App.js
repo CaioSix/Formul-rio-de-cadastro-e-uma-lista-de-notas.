82c5679d19a15b868414850a127041783f3f5bfd
Caio <caio-cavalcanti@hotmail.com>
2021-12-08 01:55:42 -0300

    feat: adicionando primeiros componentes

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import {ListaDeNotas} from './components/listaDeNotas'
+function App() {
+  return (
+    <section>
+      <form>
+        <input type="text" placeholder="Título" />
+        <textarea placeholder="Escreva sua nota"/>
+        <button> Criar Nota</button>
+      </form>
+      <ListaDeNotas/>
+    </section>
+  );
+}
+
+export default App;
